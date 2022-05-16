4ae5a470200e4cfb792deba33a9005c9bd16d107
ClaudiaBlancoM <cabm0626@gmail.com>
2022-05-16 02:15:18 -0400

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'post/index'
+  get 'post/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
