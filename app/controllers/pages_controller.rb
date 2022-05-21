eb11e9ff17adcaa2a4f91e278a77d7c690c330e7
ClaudiaBlancoM <cabm0626@gmail.com>
2022-05-21 12:19:11 -0400

    cambios efectuados a las vistas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,16 @@
 class PagesController < ApplicationController
-end
+    def index
+    end
+  
+    def create
+      @project = Project.create(name: params[:name], description: params[:description], start: params[:start], end: params[:end], state: params[:state])
+    end
+  
+    def dashboard
+      if params[:state].present?
+        @projects = Project.where('state = ?', params[:state])
+      else
+        @projects = Project.all
+      end
+    end
+  end
